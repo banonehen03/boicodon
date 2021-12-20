7df58aeb0ef2a9ad297e30ca839180450e0a9adc
banonehen03 <89727348+banonehen03@users.noreply.github.com>
2021-12-20 12:50:36 +0700

    Add files via upload

---
+++ modules/events/join.js
@@ -0,0 +1,53 @@
+﻿module.exports.config = {
+	name: "join",
+	eventType: ["log:subscribe"],
+	version: "1.0.1",
+	credits: "Mirai Team",
+	description: "Thông báo bot hoặc người vào nhóm",
+	dependencies: {
+		"fs-extra": ""
+	}
+};
+
+module.exports.run = async function({ api, event }) {
+	const { join } = global.nodemodule["path"];
+	const { threadID } = event;
+	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+		api.changeNickname(`>> ${global.config.PREFIX} << • ${(!global.config.BOTNAME) ? "𝐗𝐢𝐧 𝐜𝐡𝐚̀𝐨 𝐦𝐢̀𝐧𝐡 𝐥𝐚̀ 𝐁𝐎𝐓 𝐇𝐂𝐇𝐎𝐍𝐆, 𝐠𝐨̃ .𝐡𝐞𝐥𝐩 𝐝𝐞̂̉ 𝐱𝐞𝐦 𝐚𝐥𝐥 𝐥𝐞̣̂𝐧𝐡 𝐜𝐮̉𝐚 𝐁𝐎𝐓. \n𝐒𝐩𝐚𝐦, 𝐢𝐛 𝐁𝐎𝐓, 𝐤𝐢𝐜𝐤 𝐁𝐎𝐓 = 𝐎𝐮𝐭 𝐁𝐎𝐗, 𝐛𝐚𝐧 𝐁𝐎𝐗. \n𝐗𝐢𝐧 𝐁𝐎𝐓 𝐢𝐛 𝐀𝐝𝐦𝐢𝐧: https://www.facebook.com/profile.php?id=100052097501614. \n𝐁𝐎𝐓 𝐧𝐚̀𝐲 𝐥𝐨̣𝐜 𝐥𝐞̣̂𝐧𝐡 𝐫𝐨̂̀𝐢, 𝐱𝐢𝐧 𝐦𝐚̀ 𝐜𝐡𝐞̂ 𝐭𝐡𝐢̀ 𝐤𝐡𝐨̉𝐢 𝐬𝐚̀𝐢 = 𝐎𝐮𝐭 + 𝐁𝐚𝐧. \n𝐏𝐥𝐞𝐚𝐬𝐞 𝐮𝐬𝐞 𝐛𝐨𝐭. 𝐓𝐡𝐚𝐧𝐤" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		return api.sendMessage(`𝐗𝐢𝐧 𝐜𝐡𝐚̀𝐨 𝐦𝐢̀𝐧𝐡 𝐥𝐚̀ 𝐁𝐎𝐓 𝐇𝐂𝐇𝐎𝐍𝐆, 𝐠𝐨̃ .𝐡𝐞𝐥𝐩 𝐝𝐞̂̉ 𝐱𝐞𝐦 𝐚𝐥𝐥 𝐥𝐞̣̂𝐧𝐡 𝐜𝐮̉𝐚 𝐁𝐎𝐓. \n𝐒𝐩𝐚𝐦, 𝐢𝐛 𝐁𝐎𝐓, 𝐤𝐢𝐜𝐤 𝐁𝐎𝐓 = 𝐎𝐮𝐭 𝐁𝐎𝐗, 𝐛𝐚𝐧 𝐁𝐎𝐗. \n𝐗𝐢𝐧 𝐁𝐎𝐓 𝐢𝐛 𝐀𝐝𝐦𝐢𝐧: https://www.facebook.com/profile.php?id=100052097501614. \n𝐁𝐎𝐓 𝐧𝐚̀𝐲 𝐥𝐨̣𝐜 𝐥𝐞̣̂𝐧𝐡 𝐫𝐨̂̀𝐢, 𝐱𝐢𝐧 𝐦𝐚̀ 𝐜𝐡𝐞̂ 𝐭𝐡𝐢̀ 𝐤𝐡𝐨̉𝐢 𝐬𝐚̀𝐢 = 𝐎𝐮𝐭 + 𝐁𝐚𝐧. \n𝐏𝐥𝐞𝐚𝐬𝐞 𝐮𝐬𝐞 𝐛𝐨𝐭. 𝐓𝐡𝐚𝐧𝐤`, threadID);
+	}
+	else {
+		try {
+			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+
+			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+			const path = join(__dirname, "cache", "joinMp4");
+			const pathGif = join(path,`hi.mp4`);
+
+			var mentions = [], nameArray = [], memLength = [], i = 0;
+			
+			for (id in event.logMessageData.addedParticipants) {
+				const userName = event.logMessageData.addedParticipants[id].fullName;
+				nameArray.push(userName);
+				mentions.push({ tag: userName, id });
+				memLength.push(participantIDs.length - i++);
+			}
+			memLength.sort((a, b) => a - b);
+			
+			(typeof threadData.customJoin == "undefined") ? msg = "𝕮𝖍à𝖔 𝕸ừ𝖓𝖌 𝕰𝖒 𝖄ê𝖚 {name}.\n 𝕥ớ𝕚 𝕧ớ𝕚 𝕟𝕙ó𝕞 {threadName}.\n{type} 𝕝à 𝕖𝕞 𝕚𝕦 𝕥𝕙ứ {soThanhVien} 𝕔ủ𝕒 𝔹𝕆𝕋 🥲" : msg = threadData.customJoin;
+			msg = msg
+			.replace(/\{name}/g, nameArray.join(', '))
+			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'bạn')
+			.replace(/\{soThanhVien}/g, memLength.join(', '))
+			.replace(/\{threadName}/g, threadName);
+
+			if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+			else formPush = { body: msg, mentions }
+
+			return api.sendMessage(formPush, threadID);
+		} catch (e) { return console.log(e) };
+	}
+}
